5456fdf0c7df0d4b07ea0f0a9665c34c8713edca
Maggie Lerman <lerman.maggiel@gmail.com>
2022-02-02 07:30:46 -0500

    upgrades to package & about edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,12 @@
 module.exports = {
-  mode: 'jit',
-  purge: {
-  
-      content: [
-                './src/site/*.njk',
-                './src/site/**/*.njk',
-                './src/site/**/**/*.njk',
-                './src/site/**/**/**/*.njk'
-                ],
-      safelist:
-                [
-                  '',
-                  
-                ]
- 
-              },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-      backgroundColor: ['responsive', 'hover', 'focus', 'focus-within', 'active'],
-      borderColor: ['focus', 'hover', 'active'],
-    extend: {
-      fill: ['hover', 'focus', 'active'],
-      textDecoration: ['active'],
-      },
-  },
-  plugins: [require('@tailwindcss/typography')
+  content: [
+    './src/site/*.njk',
+    './src/site/**/*.njk',
+    './src/site/**/**/*.njk',
+    './src/site/**/**/**/*.njk'
   ],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')]
 }
