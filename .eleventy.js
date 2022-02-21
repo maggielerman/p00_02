f0357df616fdce8a38a8dc2d08a83f79e69ad339
Maggie Lerman <lerman.maggiel@gmail.com>
2022-02-21 08:42:35 -0500

    deleting unused files. changing folder names

--- .eleventy.js
+++ .eleventy.js
@@ -150,7 +150,7 @@ module.exports = function(eleventyConfig) {
 
   // static assets to pass through
   eleventyConfig.addPassthroughCopy("./src/site/images");
-  eleventyConfig.addPassthroughCopy("./src/site/assets/css/*.css");
+  // eleventyConfig.addPassthroughCopy("./src/site/assets/css/*.css");
 
   eleventyConfig.addNunjucksFilter('dateDisplay', function (date) {
     return date.toISOString()
